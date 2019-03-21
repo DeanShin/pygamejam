cf88583c5f5cefa61a066b62e90dac69e3b1d5b1
Dean Shin <42852604+ShinkaiSetsuna@users.noreply.github.com>
2019-03-21 09:59:20 -0700

    Add files via upload

---
+++ game/button.py
@@ -0,0 +1,32 @@
+class Button():
+
+    def __init__(self, pygame, fontsize, x, y, w, h, ic, hc, ac, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.text = text
+        self.font = pygame.font.Font("./resources/fonts/SoukouMincho.ttf", fontsize)
+        self.text_surface = self.font.render(text, True, ic, (63,63,63))
+        self.ac = ac
+        self.hc = hc
+        self.ic = ic
+        self.color = ic
+        self.thickness = 2
+        self.text_pos = ((x+(w/2)-self.text_surface.get_width()/2), (y+(h/2)-self.text_surface.get_height()/2))
+        self.active = False
+
+    def handle_event(self, pygame, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.color = self.ac
+
+    def update(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            mouse_hovering = True
+        else:
+            mouse_hovering = False
+        self.color = self.hc if mouse_hovering else self.ic
+
+    def draw(self, pygame, window)
+        # Blit the rect.
+        pygame.draw.rect(window, self.color, self.rect, self.thickness)
+        # Blit the text.
+        window.blit(self.text_surface, self.text_pos)
