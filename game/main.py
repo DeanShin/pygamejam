878bfb30c0960bf33a3d91ef59b270a9bb310498
Ross Mauck <ross@rossmauck.com>
2019-03-21 15:32:44 -0700

    Update main.py with event loop
    added event loop to update position of game jam text

--- game/main.py
+++ game/main.py
@@ -1,32 +1,43 @@
 import pygame
+import sys
+import os
 from pygame.locals import K_ESCAPE, K_RETURN, K_w, K_a, K_s, K_d
 
 
 pygame.init()
-pygame.font.init()
+pygame.font.init()         
+print(pygame.font.get_fonts())
 pygame.display.set_caption('Money')
-window_dims = (1760, 990)
+window_dims = (900 , 600)
 window = pygame.display.set_mode(window_dims)
 background = (63,63,63)
 FPS = 30.0
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()   
 
-fontpath = "./Dosis-Regular.ttf"
+fontpath = "./resources/Dosis-Regular.ttf"
 
-font_big = pygame.font.Font(fontpath, int(window_dims[1] / 9))
-text_big = font_big.render("Something", True, (255, 255, 255), background)
-
+font_big = pygame.font.SysFont('arial.ttf', int(window_dims[1] / 20))
+text_big = font_big.render("GAME JAM", True, (255, 255, 255), background)
+textpos = [window_dims[0] // 2, window_dims[1] // 2]
 # GAME EVENT LOOP
+game = True
+while game:
+  #event loop
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      game = False
+    if event.type == pygame.KEYDOWN:
+      if event.key == K_w:
+        textpos[1]-=10
+      if event.key == K_a:
+        textpos[0]-=10
+      if event.key == K_s:
+        textpos[1]+=10
+      if event.key == K_d:
+        textpos[0]+=10
 
-while True:
-    pygame.display.flip()
-    window.fill(background)
-    window.blit(text_big,(63,63))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        print(event)
-
-    pygame.display.update()
-    clock.tick(FPS)
+  # Draw the background
+  pygame.display.flip()
+  clock.tick(FPS)
+  window.fill(background)
+  window.blit(text_big, tuple(textpos))
