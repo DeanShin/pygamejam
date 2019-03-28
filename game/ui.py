4ade9716e561a250261efa19cb29a2e8ffda59ec
Dean Shin <42852604+ShinkaiSetsuna@users.noreply.github.com>
2019-03-28 09:49:25 -0700

    rect_sects

--- game/ui.py
+++ game/ui.py
@@ -6,16 +6,16 @@ class UI():
         self.surface = pygame.Surface((900,600))
         self.bgcolor = (15,15,255)
         self.olcolor = (255,255,255)
-        self.s1_rect = (5,5,190,390)
-        self.s2_rect = (5,405,190,190)
+        self.rect_sects = []
+        self.rect_sects.append((5,5,190,390))
+        self.rect_sects.append((5,405,190,190))
         self.gens = []
         pass
     def update(self):
         pass
     def draw(self, pygame, main_surface):
-        pygame.draw.rect(self.surface, self.bgcolor, self.s1_rect)
-        pygame.draw.rect(self.surface, self.olcolor, self.s1_rect, 10)
-        pygame.draw.rect(self.surface, self.bgcolor, self.s2_rect)
-        pygame.draw.rect(self.surface, self.olcolor, self.s2_rect, 10)
+        for rect_sect in self.rect_sects:
+            pygame.draw.rect(self.surface, self.bgcolor, rect_sect)
+            pygame.draw.rect(self.surface, self.olcolor, rect_sect, 10)
         
         main_surface.blit(self.surface, (0,0))
