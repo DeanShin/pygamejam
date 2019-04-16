c5526adbf37ff8b098da7fa8a35522fcb3de418c
Ross Mauck <ross@rossmauck.com>
2019-04-16 09:47:37 -0700

    Integrated format int and fixed money subtraction

--- game/global_functions.py
+++ game/global_functions.py
@@ -5,12 +5,11 @@ def format_int(x):
     x_str = str(x)
     num_of_digits = len(x_str)
     dot_pos = num_of_digits - ((num_of_digits - 1) // 3 * 3)
-    if num_of_digits < 4: print(x_str)
+    if num_of_digits < 4: return x_str
     else:
         x_str = x_str[:dot_pos] + "." + x_str[dot_pos:dot_pos+2] + get_letter(num_of_digits)
-        print(x_str)
+        return x_str
 def get_letter(num_of_digits):
-    #IN PROGRESS
     if num_of_digits < 7: return "K"
     elif num_of_digits < 10: return "M"
     elif num_of_digits < 13: return "B"
